13b76920f96818aad1c18505dce595c7a2705b02
Ted Shaffer <ted@brightsign.biz>
2020-12-05 09:46:54 -0800

    cleanup

--- src/controllers/dbInterface.ts
+++ src/controllers/dbInterface.ts
@@ -2,38 +2,8 @@ import { GoogleMediaItem, DbMediaItem } from '../types';
 import Mediaitem from '../models/Mediaitem';
 
 export const addMediaItemsToDb = (googleMediaItems: GoogleMediaItem[]): Promise<any> => {
-
   const dbMediaItems: DbMediaItem[] = convertGoogleMediaItemsToDbMediaItems(googleMediaItems);
-
-  // const addNextMediaItem = (index: number): Promise<void> => {
-  //   if (index >= dbMediaItems.length) {
-  //     return Promise.resolve();
-  //   }
-  //   const dbMediaItem = dbMediaItems[index];
-  //   console.log('add media item');
-  //   const promise = Mediaitem.collection.insertOne(dbMediaItem);
-  //   return promise
-  //     .then(() => {
-  //       console.log('added media item');
-  //       return addNextMediaItem(index + 1);
-  //     }).catch((error: Error) => {
-  //       console.log(error);
-  //       return Promise.reject();
-  //     });
-  // };
-
-  // return addNextMediaItem(0);
-
   return Mediaitem.collection.insertMany(dbMediaItems);
-
-  // const promise = Mediaitem.collection.insertOne(dbMediaItems[2]);
-  // promise
-  //   .then((x: any) => {
-  //     console.log('success');
-  //   }).catch((err: any) => {
-  //     console.log('failure');
-  //     console.log(err);
-  //   });
 };
 
 const convertGoogleMediaItemsToDbMediaItems = (googleMediaItems: GoogleMediaItem[]): DbMediaItem[] => {
